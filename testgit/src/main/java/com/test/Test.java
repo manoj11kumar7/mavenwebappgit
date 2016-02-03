27acebbdb7f1224b2d12906cfe023bfe320a2541
manoj.surasani <manoj.surasani@BDC4C-L-5464T12.dir.svc.accenture.com>
2016-02-03 14:53:03 +0530

    sysntax correction and the build should be success.

--- testgit/src/main/java/com/test/Test.java
+++ testgit/src/main/java/com/test/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		system.out.println("this is for testing");
+		System.out.println("this is for testing");
 
 	}
 
