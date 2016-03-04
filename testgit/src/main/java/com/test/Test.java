54c56ce2026dc5d244161acbffe536541b7f77c1
manojsurasani <manoj.surasani@BDC4C-L-5464T12.dir.svc.accenture.com>
2016-03-04 19:37:50 +0530

    build success

--- testgit/src/main/java/com/test/Test.java
+++ testgit/src/main/java/com/test/Test.java
@@ -3,7 +3,7 @@ package com.test;
 public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		system.out.println("this  is failed for testing");
+		System.out.println("this  is Success for testing");
 		System.out.println("this is for manoj testing11");
 	}
 }
